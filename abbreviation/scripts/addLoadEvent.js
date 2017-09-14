9fda9fb469baec1fa0a24d322ce83af51a817f2c
lxxtree <630374347@qq.com>
2017-09-14 18:41:24 +0800

    Create addLoadEvent.js

---
+++ abbreviation/scripts/addLoadEvent.js
@@ -0,0 +1,14 @@
+/* Created by lixx on 2017/4/23. */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+
